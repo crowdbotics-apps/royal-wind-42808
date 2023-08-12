5b23511a27e9d5ff903395a6aa8768d70cb1ba54
Crowdbotics <team@crowdbotics.com>
2023-08-12 18:04:32 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "royal-wind-42808",
+  url: "https://royal-wind-42808.botics.co",
+  api: "https://royal-wind-42808.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
